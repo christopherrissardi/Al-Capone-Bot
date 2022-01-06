a56b2c760b1f7d3fbb5ca26b71f91a57d8872a4c
Victor Nogueira <69355100+victorftrdba@users.noreply.github.com>
2022-01-06 15:24:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ client = commands.Bot(command_prefix="/", case_insensitive=False, help_command=N
 
 @client.event
 async def on_ready():
-    activity = discord.Game(name='Artic Script v1', type=3)
+    activity = discord.Game(name='Pinga com Polenta 🥃', type=3)
     await client.change_presence(status=discord.Status.dnd, activity=activity)
     print("Conectando, por favor, aguarde...")
 
