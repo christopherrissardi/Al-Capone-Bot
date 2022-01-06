a30c8cd586f0720fe142db700188d1597b99bfb5
Cristopher Rissardi <cristopherrissardi10@gmail.com>
2022-01-06 14:31:53 -0300

    Corrigido valores name na função git

--- main.py
+++ main.py
@@ -829,13 +829,13 @@ async def tradutor(ctx, phrase, *, lang):
 @client.command()
 async def git(ctx):
     embed = discord.Embed(
-        title='',
+        title='➢ CONHEÇA O REPOSITÓRIO DOS DESENVOLVEDORES',
         colour=29695
     )
     
     embed.set_thumbnail(url="https://img.icons8.com/ios-glyphs/60/ffffff/github.png")
 
-    embed.add_field(name=f"➢ CONHEÇA O REPOSITÓRIO DOS DESENVOLVEDORES", value=f"https://github.com/victorftrdba | https://github.com/ALIENxp", inline=False)
+    embed.add_field(name=f"LINKS", value=f"el Marlboro#8779: https://github.com/victorftrdba \nALIEN#7278: https://github.com/ALIENxp", inline=False)
 
     embed.set_footer(text='Requested By {}'.format(ctx.author), icon_url=ctx.author.avatar_url)
    
