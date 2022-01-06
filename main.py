36aedd25adb0e617d8bb672d5e85ad1a1e7d8d51
Victor Nogueira <69355100+victorftrdba@users.noreply.github.com>
2022-01-06 01:40:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -796,7 +796,7 @@ async def traduzir(ctx, phrase, *, lang):
 
     translator = Translator()
 
-    phrase_translate = translator.translate(f"{phrase}", dest=lang if lang != null else "en")
+    phrase_translate = translator.translate(f"{phrase}", dest=lang if lang != "en" else "en")
 
     embed.add_field(name="➢ TEXTO TRADUZIDO", value=f"{phrase_translate.text}", inline=False)
 
