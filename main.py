5ca51eb3ff9c981acd1e7cc22436e18fa0bbefb8
Victor Nogueira <69355100+victorftrdba@users.noreply.github.com>
2022-01-06 02:08:10 -0300

    Update main.py

--- main.py
+++ main.py
@@ -797,9 +797,7 @@ async def traduzir(ctx):
     
     embed.set_thumbnail(url="https://cdn.iconscout.com/icon/free/png-256/languages-1891105-1598018.png")
 
-    embed.add_field(name="➢ COMANDO", value='***/tradutor "Texto" SELECIONE UMA LÍNGUA (Exemplo: en, es, pt)***', inline=False)
-    
-    embed.set_author(name=f"ㅤㅤㅤㅤㅤㅤㅤㅤ➢ COMO TRADUZIR?ㅤㅤㅤㅤㅤㅤㅤㅤ", icon_url='')
+    embed.add_field(name="➢ COMANDO PARA TRADUÇÃO", value='***/tradutor "Texto" SELECIONE UMA LÍNGUA (Exemplo: en, es, pt)***', inline=False)
 
     embed.set_footer(text='Requested By {}'.format(ctx.author), icon_url=ctx.author.avatar_url)
 
